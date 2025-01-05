c1fa10dfb656a40f88623d0212059badd8778009
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2025-01-04 21:18:25 -0500

    feat: add skeleton for basic CLI task interface

--- TaskTracker/TaskTracker/src/Program.cs
+++ TaskTracker/TaskTracker/src/Program.cs
@@ -1,17 +1,72 @@
 ﻿Console.WriteLine("Task Tracker CLI Application");
 
-// while (true)
-// {
-//     Console.WriteLine("Enter a task to add to your list: ");
-//     var newTask = Convert.ToString(Console.ReadLine());
-//     if (newTask is not null)
-//     {
-//         taskList.Add(newTask);
-//     }
-// }
-
 var tasks = new TaskManager.Tasks();
-tasks.AddTask("Brush teeth", "Make breakfast", "Make bed");
-tasks.RemoveTaskByName("Brush teeth");
-tasks.RemoveTaskByIndex(0);
-tasks.DisplayTasks();
+
+while (true)
+{
+    var rawInput = Console.ReadLine();
+
+    string command;
+    string argument;
+
+    if (rawInput is not null)
+    {
+        var indexOfSpace = rawInput.IndexOf(' ');
+        if (indexOfSpace != -1)
+        {
+            command = rawInput[..indexOfSpace];
+            argument = rawInput[(indexOfSpace + 1)..];
+        }
+        else
+        {
+            command = rawInput;
+            argument = "";
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please supply a non-null command, type `help` for valid commands.");
+        continue;
+    }
+
+    Console.WriteLine("[DEBUG]: `{cmd: " + command + "} {arg: " + argument + "}`");
+
+    switch(command)
+    {
+        case "add":
+            break;
+        case "update":
+            break;
+        case "delete":
+            break;
+        case "view":
+            break;
+        case "inprogress":
+            break;
+        case "complete":
+            break;
+        case "exit":
+            System.Environment.Exit(1);
+            break;
+        case "help":
+            Console.WriteLine(@"
+            add <task>
+            update <task/index>
+            delete <task/index>
+            view
+            inprogress <task/index>
+            complete <task/index>
+            ");
+            break;
+        default:
+            Console.WriteLine("Invalid input, type `help` for valid commands.");
+            continue;
+    }
+}
+
+// add <task>
+// update <task/index>
+// delete <task/index>
+// view
+// inprogress <task/index>
+// complete <task/index>
