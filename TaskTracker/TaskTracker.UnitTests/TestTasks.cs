bf510c24831134b3a550cdb0daf76ef2d76ebdff
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2025-01-04 20:30:50 -0500

    refactor: move tests to seperate project TaskTracker.UnitTests

---
+++ TaskTracker/TaskTracker.UnitTests/TestTasks.cs
@@ -0,0 +1,108 @@
+﻿using TaskManager;
+
+namespace TaskTracker.UnitTests;
+
+public class TestTasks
+{
+    [Fact]
+    public void CanAddTask()
+    {
+        var task = new Tasks();
+
+        task.AddTask("x");
+
+        var taskContents = task.GetTaskContents();
+        Assert.Contains("x", taskContents);
+    }
+
+    [Fact]
+    public void CanAddMultipleTask()
+    {
+        var task = new Tasks();
+
+        task.AddTask("x", "y");
+
+        var taskContents = task.GetTaskContents();
+        Assert.Contains("x", taskContents);
+        Assert.Contains("y", taskContents);
+    }
+
+    [Fact]
+    public void CanRemoveTaskByIndex()
+    {
+        var task = new Tasks();
+
+        task.AddTask("x");
+        task.AddTask("y");
+        task.AddTask("z");
+
+        task.RemoveTaskByIndex(1); // Removes y
+
+        var taskContents = task.GetTaskContents();
+        Assert.DoesNotContain("y", taskContents);
+    }
+
+    [Fact]
+    public void CanRemoveMultipleTaskByIndex()
+    {
+        var task = new Tasks();
+
+        task.AddTask("x");
+        task.AddTask("y");
+        task.AddTask("z");
+
+        task.RemoveTaskByIndex(1, 2); // Removes y and z
+
+        var taskContents = task.GetTaskContents();
+        Assert.DoesNotContain("y", taskContents);
+        Assert.DoesNotContain("z", taskContents);
+    }
+
+    [Fact]
+    public void CanRemoveTaskByName()
+    {
+        var task = new Tasks();
+
+        task.AddTask("x");
+        task.AddTask("y");
+        task.AddTask("z");
+
+        task.RemoveTaskByName("z");
+
+        var taskContents = task.GetTaskContents();
+        Assert.DoesNotContain("z", taskContents);
+    }
+
+    [Fact]
+    public void CanRemoveMultipleTaskByName()
+    {
+        var task = new Tasks();
+
+        task.AddTask("x");
+        task.AddTask("y");
+        task.AddTask("z");
+
+        task.RemoveTaskByName("x");
+        task.RemoveTaskByName("z");
+
+        var taskContents = task.GetTaskContents();
+        Assert.DoesNotContain("x", taskContents);
+        Assert.DoesNotContain("z", taskContents);
+    }
+
+    [Fact]
+    public void DisplayTasksOutputsAllTasks()
+    {
+        var task = new Tasks();
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        task.AddTask("x");
+        task.AddTask("$c0nta1ns$");
+        task.AddTask("z");
+        task.DisplayTasks();
+
+        var output = stringWriter.ToString().Trim();
+        Assert.Contains("$c0nta1ns$", output);
+    }
+}
--- TaskTracker/TaskTracker/src/Tasks.cs
+++ TaskTracker/TaskTracker/src/Tasks.cs
@@ -1,83 +1,82 @@
-namespace TaskManager
+namespace TaskManager;
+
+public enum TaskStatus
 {
-    enum TaskStatus
-    {
-        Incomplete,
-        Active,
-        Done
-    }
-
-    struct Task(string content, TaskStatus status)
-    {
-        public string Content {get; set;} = content;
-        public TaskStatus Status {get; set;} = status;
-    }
-
-    class Tasks
-    {
-        public List<Task> TaskList = [];
-
-        public Tasks() {}
-
-        /// <summary>
-        /// Adds an arbitrary amount of tasks to the list.
-        /// </summary>
-        /// <param name="tasks">The tasks to add.</param>
-        public void AddTask(params string[] tasks)
-        {
-            foreach (var task in tasks)
-            {
-                TaskList.Add(new Task(task, TaskStatus.Incomplete));
-            }
-        }
-
-        /// <summary>
-        /// Removes an arbitrary amount of tasks by indices.
-        /// </summary>
-        /// <param name="indices">The tasks to remove given their indices.</param>
-        public void RemoveTaskByIndex(params int[] indices)
-        {
-            var count = 0;
-            foreach (var index in indices)
-            {
-                // When an element from a list is deleted all
-                // index positions are shifted by the current
-                // iteration - 1
-                TaskList.RemoveAt(index - count);
-                count++;
-            }
-        }
-
-        /// <summary>
-        /// Removes an arbitrary amount of tasks from the list by name/value.
-        /// </summary>
-        /// <param name="task">The tasks to remove given their names.</param>
-        public void RemoveTaskByName(params string[] tasks)
-        {
-            foreach(var task in tasks)
-            {
-                TaskList = TaskList.Where(t => t.Content != task).ToList();
-            }
-        }
-
-        /// <summary>
-        /// Returns a list containing only the contents portion of all tasks.
-        /// </summary>
-        /// <returns>A contents list.</returns>
-        public List<string> GetTaskContents()
-        {
-            return TaskList.Select(task => task.Content).ToList();
-        }
-
-        /// <summary>
-        /// Displays all the tasks to do.
-        /// </summary>
-        public void DisplayTasks()
-        {
-            for (var i = 0; i < TaskList.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}) {TaskList[i].Content}");
-            }
-        }
-    };
+    Incomplete,
+    Active,
+    Done
 }
+
+public struct Task(string content, TaskStatus status)
+{
+    public string Content {get; set;} = content;
+    public TaskStatus Status {get; set;} = status;
+}
+
+public class Tasks
+{
+    public List<Task> TaskList = [];
+
+    public Tasks() {}
+
+    /// <summary>
+    /// Adds an arbitrary amount of tasks to the list.
+    /// </summary>
+    /// <param name="tasks">The tasks to add.</param>
+    public void AddTask(params string[] tasks)
+    {
+        foreach (var task in tasks)
+        {
+            TaskList.Add(new Task(task, TaskStatus.Incomplete));
+        }
+    }
+
+    /// <summary>
+    /// Removes an arbitrary amount of tasks by indices.
+    /// </summary>
+    /// <param name="indices">The tasks to remove given their indices.</param>
+    public void RemoveTaskByIndex(params int[] indices)
+    {
+        var count = 0;
+        foreach (var index in indices)
+        {
+            // When an element from a list is deleted all
+            // index positions are shifted by the current
+            // iteration - 1
+            TaskList.RemoveAt(index - count);
+            count++;
+        }
+    }
+
+    /// <summary>
+    /// Removes an arbitrary amount of tasks from the list by name/value.
+    /// </summary>
+    /// <param name="task">The tasks to remove given their names.</param>
+    public void RemoveTaskByName(params string[] tasks)
+    {
+        foreach(var task in tasks)
+        {
+            TaskList = TaskList.Where(t => t.Content != task).ToList();
+        }
+    }
+
+    /// <summary>
+    /// Returns a list containing only the contents portion of all tasks.
+    /// </summary>
+    /// <returns>A contents list.</returns>
+    public List<string> GetTaskContents()
+    {
+        return TaskList.Select(task => task.Content).ToList();
+    }
+
+    /// <summary>
+    /// Displays all the tasks to do.
+    /// </summary>
+    public void DisplayTasks()
+    {
+        for (var i = 0; i < TaskList.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}) {TaskList[i].Content}");
+        }
+    }
+};
